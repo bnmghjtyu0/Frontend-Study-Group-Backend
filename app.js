8ab5465e3b489a3279d23fdddcccd3600e33e2cb
RichardLiao <bnmghjtyu0@gmail.com>
2020-01-09 16:23:32 +0800

    update

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,23 +18,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', function (req, res, next) {
+  res.status(200).json({ title: 'Express' });
+});
+
+app.use('/backend', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const parse = require('csv-parse')
+const axios = require('axios')
+const googleSheet = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQf-z7Z9vn6MDmQPO25aamsdAV6oORGDTHhVO_n-CvMdPtKykuH5RDJ9dAJeQkJEUTTHmANmznPeM3C/pub?gid=0&output=csv'
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/articles', async (req, res, next) => {
+  const getCsv = async url => {
+    const res = await axios.get(url)
+    return await new Promise((resolve, reject) => {
+      parse(res.data, { columns: true }, (err, output) => err ? reject(err) : resolve(output))
+    })
+  }
+  const datas = await getCsv(googleSheet)
+  console.log(datas)
+  res.status(200).json({ retCode: 1, retMsg: '', retData: datas });
 });
 
 module.exports = router;
