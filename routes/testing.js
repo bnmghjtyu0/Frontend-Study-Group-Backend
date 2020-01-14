3b63568ff6424643eaabd222abf403e26f4a0184
RichardLiao <bnmghjtyu0@gmail.com>
2020-01-14 11:32:16 +0800

    test

--- routes/testing.js
+++ routes/testing.js
@@ -7,7 +7,7 @@ router.get('/1', async (req, res, next) => {
 })
 
 router.get('/2', async (req, res, next) => {
-  await setTimeout(() => res.status(200).json({ text: 'this is testing 2' }), 3000)
+  await setTimeout(() => res.status(200).json({ text: 'this is testing 22222222222' }), 3000)
 })
 
 module.exports = router
